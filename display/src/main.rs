4c0957e134b471993bccc7a7ee3365d47df24c23
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-28 23:22:56 -0700

    Update display.

--- display/src/main.rs
+++ display/src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::fmt::{self, Write};
 
 struct Toast(String);
 
@@ -8,7 +8,36 @@ impl fmt::Display for Toast {
     }
 }
 
+enum Shape {
+    Circle,
+    Diamond,
+    Square,
+    Star,
+    Triangle,
+}
+
+impl fmt::Display for Shape {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Shape::Circle => f.write_char('●'),
+            Shape::Diamond => f.write_char('◆'),
+            Shape::Square => f.write_char('■'),
+            Shape::Star => f.write_char('★'),
+            Shape::Triangle => f.write_char('▲'),
+        }
+    }
+}
+
 fn main() {
     let t = Toast("Avocado toast".to_string());
     println!("{}", t);
+
+    let shapes: &[Shape; 5] = &[
+        Shape::Circle,
+        Shape::Diamond,
+        Shape::Square,
+        Shape::Star,
+        Shape::Triangle,
+    ];
+    shapes.iter().for_each(|s| println!("{}", s));
 }
